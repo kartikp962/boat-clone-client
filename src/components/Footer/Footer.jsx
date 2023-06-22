179b273b08595972b1868e4e7867816e79c64a74
kartikp962 <kpal87332@gmail.com>
2023-06-22 16:42:29 +0530

    client files added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
+
+const Footer = () => {
+    return <footer className="footer">
+        <div className="footer-content">
+            <div className="col">
+                <div className="title">About</div>
+                <div className="text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, suscipit.
+                </div>
+            </div>
+            <div className="col">
+                <div className="title">Contact</div>
+                <div className="c-item">
+                    <FaLocationArrow />
+                    <div className="text">
+                        Kayalorram Rd, Punnamada, Kottakulangara, Atappuzha, kerala, 688006
+                    </div>
+                </div>
+
+                <div className="c-item">
+                    <FaMobileAlt />
+                    <div className="text">
+                    Phone: 0471 272 0261
+                    </div>
+                </div>
+
+                <div className="c-item">
+                    <FaEnvelope />
+                    <div className="text">
+                        Email: store@boat.com
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="title">Categories</div>
+                <span className="text">Headphones</span>
+                <span className="text">Smart Watches</span>
+                <span className="text">Bluetooth Speakers</span>
+                <span className="text">Wireless Earbuds</span>
+                <span className="text">Home Theatre</span>
+                <span className="text">Projectors</span>
+            </div>
+            <div className="col">
+                <div className="title">Pages</div>
+                <span className="text">Home</span>
+                <span className="text">About</span>
+                <span className="text">Privacy Policy</span>
+                <span className="text">Returns</span>
+                <span className="text">Terms & Conditions</span>
+                <span className="text">Contact Us</span>
+            </div>
+        </div>
+        <div className="bottom-bar">
+            <div className="bottom-bar-content">
+                <span className="text">
+                    BOAT CLONE 2023 CREATED BY KARTIK PAL. PREMIUM E-COMMERCE SOLUTIONS
+                </span>
+                <img src={Payment} alt="" />
+            </div>
+        </div>
+    </footer>;
+};
+
+export default Footer;
